41aab2177c369fea8a2af275721590e7e9118570
Alza Ichsan K <alzaichsank@gmail.com>
2020-09-21 22:45:12 +0700

    add back button handler for exit

--- app/src/main/java/alzaichsank/com/simple_note/presenter/MainActivity.kt
+++ app/src/main/java/alzaichsank/com/simple_note/presenter/MainActivity.kt
@@ -1,13 +1,29 @@
 package alzaichsank.com.simple_note.presenter
 
 import alzaichsank.com.simple_note.R
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-
+    var doubleBackToExitPressedOnce = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
     }
 }
